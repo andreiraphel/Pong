7f84332e565e7969cb781a83118384794c4a71ef
Andrei Raphel Amang <andreiraphel@gmail.com>
2024-04-14 21:18:05 +0800

    Added player 1

---
+++ player_1.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _draw():
+	draw_rect(Rect2(0, 0, 100, 50), Color(1, 1, 1))
