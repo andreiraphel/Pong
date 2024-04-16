9b2f3fc0cd3378ea8c3aa8f77d3a89388a027602
Andrei Raphel Amang <andreiraphel@gmail.com>
2024-04-16 21:19:51 +0800

    Added ball

--- ball.gd
+++ ball.gd
@@ -3,11 +3,13 @@ extends RigidBody2D
 const INITIAL_SPEED = 200
 var initial_direction = Vector2.RIGHT
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	apply_impulse(Vector2(), initial_direction * INITIAL_SPEED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	apply_impulse(Vector2(), initial_direction * INITIAL_SPEED)
+	var viewport = get_viewport()
+	var viewport_rect = viewport.get_visible_rect()
+	
+	position += initial_direction * INITIAL_SPEED * delta
+
+	position = clamp(position, 0, viewport_rect.size - 100)
